2f4cb4e19b5a07edab91e088e1e7f1ff5220777e
VejAlDatt <126171063+VejAlDatt@users.noreply.github.com>
2023-06-23 07:37:47 -0700

    Add files via upload

---
+++ src/deps/connConfiguration.py
@@ -0,0 +1,5 @@
+# Configuration for connecting to the PostgreSQL database
+host = 'database'  # The hostname or IP address of the PostgreSQL server
+database = 'database'  # The name of the PostgreSQL database
+login = 'datapipeline'  # The login username for accessing the database
+pw = 'datapipeline'  # The password for the login username
